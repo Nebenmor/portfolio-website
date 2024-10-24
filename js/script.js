7e53b3c08be2189f4eacb2b1ad044b49e0b27eff
Nebenmor <nebenmor.anthony@gmail.com>
2024-10-24 19:57:51 +0100

    implement typed animation

--- js/script.js
+++ js/script.js
@@ -53,3 +53,10 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Graphic Designer', 'Digital Marketer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
