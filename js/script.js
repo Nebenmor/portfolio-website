a68fac12baa7a0a930a6a0ef27df806225a6c74b
Nebenmor <nebenmor.anthony@gmail.com>
2024-10-24 19:01:17 +0100

    implement update for RWD

--- js/script.js
+++ js/script.js
@@ -34,7 +34,9 @@ window.onscroll = () => {
     header.classList.toggle('sticky', window.scrollY > 100);
     
     /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
-};
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};  
 
 
 /*==================== scroll reveal ====================*/ 
