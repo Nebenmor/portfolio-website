8fca6bc22787bb897e5cf80fab03ba6422c239d8
Nebenmor <nebenmor.anthony@gmail.com>
2024-12-06 20:25:20 +0100

    implement update

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Graphic Designer', 'Digital Marketer'],
+    strings: ['Frontend Developer', 'Backend Developer', 'Web Designer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
